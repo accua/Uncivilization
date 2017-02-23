5fa0d74ac01aab85de74856ac036a54522abac78
Adam Zinder <adam.zinder@gmail.com>
2017-02-23 07:27:50 -0800

    seed database and set up ajax architecture

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,11 +1,15 @@
 class EventsController < ApplicationController
 
   def show
-  @nation = Nation.find(params[:nation_id])
-  @events = Events.all
-  @event = @events.sample
-
-  respond_to do |format|
-    format.html { redirect_to user_nation_path(current_user, @nation) }
+    @nation = Nation.find(params[:nation_id])
+    @events = Event.all
+    @event = @events.sample
+    @responses = Response.all
+    @response1 = @responses.sample
+    @response2 = @responses.sample
+    respond_to do |format|
+      format.html { redirect_to user_nation_path(current_user, @nation) }
+      format.js
+    end
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,7 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+event = Event.create(content: "Through hardwork and innovation a team of scientists  in the great nation of <%= @nation.nation_name @> have finally found a cure to cancer.  What decision do you you decide to make?")
+
+response = Response.create([
+{content: "Do nothing and hope for the best!", event_id: 1, nation_id: 1, population: 2000, lumber: 3, stability: 10, metal: 3, oil: 4, capital: 30, minerals: 2},
+
+{content: "Crush the infedels!!!", event_id: 1, nation_id: 1, population: 8000, lumber: 10, oil: 10, stability: 10, metal: 4, capital: 30, minerals: 4}
+])
